32ce81afa8f36bfafe94cf5338f5a4c1f0b1d85c
Jdvd01 <velasquezdenorajose@gmail.com>
2022-04-23 00:42:22 +0000

    entrega del proyecto

--- src/models.py
+++ src/models.py
@@ -8,26 +8,70 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class Usuario(Base):
+    __tablename__ = "usuario"
+    id = Column(Integer, primary_key = True)
+    favorito = relationship("favorito")
+    
+    email = Column(String(50), nullable = False, unique = True)
+    password = Column(String(15), nullable = False, unique = False)
+    nombre = Column(String(20), nullable = False, unique = False)
+    apellido = Column(String(20), nullable = True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Favorito(Base):
+    __tablename__ = "favorito"
+    id = Column(Integer, primary_key = True)
+    usuario_id = Column(Integer, ForeignKey("usuario.id"))
+    usuario = relationship("usuario")
 
-    def to_dict(self):
-        return {}
+    planeta = relationship("planeta")
+    planeta_id = Column(Integer, ForeignKey("planeta.id"))
+
+    personaje = relationship("personaje")
+    personaje_id = Column(Integer, ForeignKey("personaje.id"))
+
+    vehiculo = relationship("vehiculo")
+    vehiculo_id = Column(Integer, ForeignKey("vehiculo.id"))
+
+    nave = relationship("nave")
+    nave_id = Column(Integer, ForeignKey("nave.id"))
+
+class Planeta(Base):
+    __tablename__ = "planeta"
+    id = Column(Integer, primary_key = True)
+    favorito = relationship("favorito")
+
+    nombre = Column(String(40), nullable = False, unique = True)
+    clima = Column(String(20), nullable = False, unique = False)
+    terreno = Column(String(20), nullable = False, unique = False)
+    poblacion = Column(Integer, nullable = True)
+
+class Personaje(Base):
+    __tablename__ = "personaje"
+    id = Column(Integer, primary_key = True)
+
+    nombre = Column(String(20), nullable = False, unique = False)
+    genero = Column(String(10), nullable = True)
+    edad = Column(Integer, nullable = False, unique = False)
+
+class Vehiculo(Base):
+    __tablename__ = "vehiculo"
+    id = Column(Integer, primary_key = True)
+
+    nombre = Column(String(30), nullable = False, unique= True)
+    modelo = Column(String(30), nullable = False, unique = True)
+    costo = Column(Integer, nullable = False, unique = False)
+    pasajeros = Column(Integer, nullable = True)
+
+
+class Nave(Base):
+    __tablename__ = "nave"
+    id = Column(Integer, primary_key = True)
+
+    nombre = Column(String(30), nullable = False, unique = False)
+    modelo = Column(String(30), nullable = False, unique = False)
+    costo = Column(Integer, nullable = False, unique = False)
+    pasajeros = Column(Integer, nullable = False, unique = False)
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
